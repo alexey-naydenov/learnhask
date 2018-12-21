db038ebc78668f49244529dc8149256ee454c9a4
Alexey Naydenov <dev@alexeynaydenov.triacontahedron.com>
2018-12-21 19:02:24 +0300

    Implement tree map and to list

--- src/MyTree.hs
+++ src/MyTree.hs
@@ -3,4 +3,20 @@ module MyTree where
 data MyBinaryTree a =
   Leaf
   | Node (MyBinaryTree a) a (MyBinaryTree a)
+  deriving (Eq, Ord, Show)
 
+mapTree :: (a -> b) -> MyBinaryTree a -> MyBinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left x right) = Node (mapTree f left) (f x) (mapTree f right)
+
+preorder :: MyBinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left x right) = [x] ++ (preorder left) ++ (preorder right)
+
+inorder :: MyBinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left x right) = (inorder left) ++ [x] ++ (inorder right)
+
+postorder :: MyBinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left x right) = (postorder left) ++ (postorder right) ++ [x]
--- test/MyTreeSpec.hs
+++ test/MyTreeSpec.hs
@@ -3,7 +3,16 @@ module MyTreeSpec (spec) where
 import Test.Hspec
 import MyTree
 
+testTree :: MyBinaryTree Int
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+incremented :: MyBinaryTree Int
+incremented = Node (Node Leaf 2 Leaf) 3 (Node Leaf 4 Leaf)
+
 spec :: Spec
 spec = do
-  describe "simple tests" $ do
-    it "" $ (2 + 2) `shouldBe` 4
+  describe "binary tree" $ do
+    it "increment" $ (mapTree (+1) testTree) `shouldBe` incremented
+    it "preorder" $ (preorder testTree) `shouldBe` [2, 1, 3]
+    it "inorder" $ (inorder testTree) `shouldBe` [1, 2, 3]
+    it "postorder" $ (postorder testTree) `shouldBe` [1, 3, 2]
